0b57bec1c260a96f3ba23098f3c4020ffac23e6f
nenjotsu <renenjo.valente@ubidy.com>
2018-12-12 09:37:57 +0800

    Updates

---
+++ precache-manifest.2042652fcc25114ca6b77dc0c6fede80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "196863a7137300c0cd9b",
+    "url": "/nenjotsu/advanced-redux-pattern/static/js/runtime~main.196863a7.js"
+  },
+  {
+    "revision": "42576fd9ec3824a3e8c7",
+    "url": "/nenjotsu/advanced-redux-pattern/static/js/main.42576fd9.chunk.js"
+  },
+  {
+    "revision": "42aaa6d71ff41a4733c5",
+    "url": "/nenjotsu/advanced-redux-pattern/static/js/1.42aaa6d7.chunk.js"
+  },
+  {
+    "revision": "42576fd9ec3824a3e8c7",
+    "url": "/nenjotsu/advanced-redux-pattern/static/css/main.d59c6eef.chunk.css"
+  },
+  {
+    "revision": "42aaa6d71ff41a4733c5",
+    "url": "/nenjotsu/advanced-redux-pattern/static/css/1.99dd4f6f.chunk.css"
+  },
+  {
+    "revision": "e71969947825f7e89b0f4e455885392b",
+    "url": "/nenjotsu/advanced-redux-pattern/index.html"
+  }
+];
